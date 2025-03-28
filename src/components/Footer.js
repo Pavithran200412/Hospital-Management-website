2a6ee898a0891b4666bf7c3c018525569f62182b
Pavithran200412 <s.pavithran.comp2020@gmail.com>
2025-03-28 15:57:01 +0530

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -40,9 +40,21 @@ const Footer = () => {
         {/* Contact & Location */}
         <div className="footer-contact">
           <h5>Location</h5>
-          <p>P.O Box 19737<br />Doha, Qatar</p>
+          <p>241, Grand Southern Trunk Rd, near MIT Bridge, Kamakshi Colony, Chromepet,<br /> Chennai, Tamil Nadu 600044</p>
           <h5>Contact Us</h5>
           <p><b>E-Mail Us</b><br /><a href="mailto:info@MedCarehospital.com">info@MedCarehospital.com</a></p>
+
+          {/* Google Map */}
+          <div className="footer-map mt-3">
+            <h5>Find Us on Google Maps</h5>
+            <iframe
+              title="Google Map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387190.2799181496!2d-74.25987571760744!3d40.69767006358627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259af18b60165%3A0x8b621f8a7a7d28a4!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2s!4v1633452834502!5m2!1sen!2s"
+              style={{ width: "100%", height: "300px", border: 0 }}
+              allowFullScreen
+              loading="lazy"
+            />
+          </div>
         </div>
       </div>
 
